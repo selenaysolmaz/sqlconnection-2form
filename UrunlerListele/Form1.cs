af073a7b8395e047d29058667bc12916c740e21b
semvs <semvs@ayvansaray.edu.tr>
2019-01-21 13:41:52 +0300

    ado.net baglantı kur ve 2 form olustur

---
+++ UrunlerListele/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace UrunlerListele
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti = new SqlConnection(ConfigurationManager.ConnectionStrings["Baglanti"].ConnectionString);
+            }
+            catch (System.ApplicationException ex)
+            {
+                Console.WriteLine("Error reading from {0}.Message= {1}", ex.Source, ex.Message);
+            }
+
+            SqlCommand komut = new SqlCommand();
+            komut.CommandText = "SELECT *FROM Products";
+            komut.Connection = baglanti;
+
+            baglanti.Open();
+
+            SqlDataReader rdr = komut.ExecuteReader();
+
+            while (rdr.Read())
+            {
+
+                object ProductName = rdr["ProductName"];
+                object Price = rdr["UnitPrice"];
+                object Stok = rdr["UnitsInStock"];
+
+                listKategori.Items.Add(ProductName + " - " + Price + "--" + Stok);
+                
+                //object ProductName = rdr[1];
+                //object Price = rdr[5];
+                //object Stok = rdr[6];
+
+
+            }
+            // rdr.Close();
+            baglanti.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kategori ktgr = new kategori();
+            ktgr.Show();
+
+            SqlCommand komut2 = new SqlCommand();
+            komut2.CommandText = "SELECT * FROM Categories";
+            baglanti.Open();
+            komut2.Connection = baglanti;
+            
+            SqlDataReader rdr2 = komut2.ExecuteReader();
+
+            while (rdr2.Read())
+            {
+
+                object CategoriName = rdr2["CategoryName"];
+                object Description = rdr2["Description"];
+                
+
+               ktgr.listKategori1.Items.Add(CategoriName + " - " + Description );
+
+                //object ProductName = rdr[1];
+                //object Price = rdr[5];
+                //object Stok = rdr[6];
+
+
+            }
+            baglanti.Close();
+        }
+    }
+}
---
+++ UrunlerListele/kategori.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace UrunlerListele
+{
+    partial class kategori
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listKategori1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listKategori1
+            // 
+            this.listKategori1.FormattingEnabled = true;
+            this.listKategori1.Location = new System.Drawing.Point(25, 39);
+            this.listKategori1.Name = "listKategori1";
+            this.listKategori1.Size = new System.Drawing.Size(276, 225);
+            this.listKategori1.TabIndex = 0;
+            // 
+            // kategori
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 300);
+            this.Controls.Add(this.listKategori1);
+            this.Name = "kategori";
+            this.Text = "kategori";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ListBox listKategori1;
+    }
+}
